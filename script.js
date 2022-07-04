501aa77dc34df15880c24e14cf874f2f4326f208
Victor Mendes <victorutdsim7@gmail.com>
2022-07-04 15:52:50 -0300

    requisito 5

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ function generateRandomColor() {
   return `rgb(${Math.random() * 240}, ${Math.random() * 240}, ${Math.random() * 240})`;
 }
 
-
 const colors = document.getElementsByClassName('color');
 for (let index = 1; index < colors.length; index += 1) {
   colors[index].style.backgroundColor = generateRandomColor();
 }
 function generatePixels() {
-  let pixelBoard = document.getElementById('pixel-board');
+  const pixelBoard = document.getElementById('pixel-board');
   for (let numberOfPixels = 1; numberOfPixels <= 25; numberOfPixels += 1) {
     let pixel = document.createElement('div');
     pixel.classList.add('pixel');
